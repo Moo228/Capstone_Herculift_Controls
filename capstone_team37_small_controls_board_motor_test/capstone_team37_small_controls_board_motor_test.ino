b18e8bd0d7a29b115c1ff74bb4370d28a55183e3
Moo228 <92613774+Moo228@users.noreply.github.com>
2023-02-28 11:13:23 -0700

    Add print statements to serial monitor output

--- capstone_team37_small_controls_board_motor_test/capstone_team37_small_controls_board_motor_test.ino
+++ capstone_team37_small_controls_board_motor_test/capstone_team37_small_controls_board_motor_test.ino
@@ -38,7 +38,7 @@
 #define ERROR_ZONE_VAL 100
 #define MOVE_ZONE_VAL 5000
 
-#define MAX_MOTOR_PWM 20
+#define MAX_MOTOR_PWM 30
 
 //Define a fake number to test our single load cell system.
 #define FAKE_LOAD_TENSION_VAL 3000.0
@@ -133,7 +133,7 @@ void loop() {
   Serial.print(",");
   Serial.print(cable_scale_reading * 1000);
   Serial.print(",");
-  Serial.println(scaledTensionHandle - cable_scale_reading * 1000);
+  Serial.println(calculateError(scaledTensionHandle, cable_scale_reading * 1000));
   
   //Adjust the motor based on the read handle sensor data.
   errorToPWM(calculateError(scaledTensionHandle, cable_scale_reading*1000));
@@ -192,19 +192,25 @@ void errorToPWM(double errorVal) {
   if(errorVal < -MOVE_ZONE_VAL) {
     //Move the motor down at the max value when the error value is too negetive (Safety feature to prevent excessive motor speed).
     moveMotor(DOWN, MAX_MOTOR_PWM);
+    Serial.println("MAX DOWN regime");
   } else if(errorVal > -MOVE_ZONE_VAL && errorVal < -ERROR_ZONE_VAL) {
     //Move the motor down at a linear rate between the (x, y) points (-MOVE_ZONE_VAL, MAX_MOTOR_PWM) and (-ERROR_ZONE_VAL, 0).
     // moveMotor(DOWN, map(errorVal, -MOVE_ZONE_VAL, -ERROR_ZONE_VAL, MAX_MOTOR_PWM, 0));
-    moveMotor(DOWN, 125);
+    moveMotor(DOWN, 30);
+    Serial.println("DOWN regime");
   } else if(errorVal > -ERROR_ZONE_VAL && errorVal < ERROR_ZONE_VAL) {
     //Stop the motor when errorVal is within a certain margin of error defined by 2 * ERROR_ZONE_VAL.
     moveMotor(NONE, 0);
+    Serial.println("NONE regime");
   } else if(errorVal > ERROR_ZONE_VAL && errorVal < MOVE_ZONE_VAL) {
     //Move the motor down at a linear rate between the (x, y) points (ERROR_ZONE_VAL, 0) and (MOVE_ZONE_VAL, MAX_MOTOR_PWM).
     // moveMotor(UP, map(errorVal, ERROR_ZONE_VAL, MOVE_ZONE_VAL, 0, MAX_MOTOR_PWM));
-    moveMotor(UP, 125);
+    moveMotor(UP, 30);
+    Serial.println("UP regime");
   } else {
     //Move the motor down at the max value when the error value is too positive (Safety feature to prevent excessive motor speed).
     moveMotor(UP, MAX_MOTOR_PWM);
+    Serial.println("MAX UP regime");
+
   }
 }
