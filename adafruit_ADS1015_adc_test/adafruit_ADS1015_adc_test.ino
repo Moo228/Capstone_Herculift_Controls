8923f6dd0b533155a685746b5b7036dfe31b17e4
Moo228 <92613774+Moo228@users.noreply.github.com>
2023-03-13 09:53:04 -0600

    Add file for initial Adafruit ADS1015 12bit ADC.

---
+++ adafruit_ADS1015_adc_test/adafruit_ADS1015_adc_test.ino
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Title: Adafruit ADS1015 ADC Test
+//  Capstone Team: 37 
+//  Authors: Tyler Dickson, Joseph Edmund, Isaac Sorensen
+//  
+//  Description: This is a body of code to test the Adafruit ADS1015 ADC breakout boards.
+//  
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/****************************************Libraries****************************************/
+#include <Adafruit_ADS1X15.h> //Include the library for the Adafruit ADS1015 boards
+
+/****************************************Macros****************************************/
+//SDA is defined as pin 18 which is A4 on the Arduino nano
+//SCL is defined as pin 19 which is A5 on the Arduino nano
+
+//These values are defined in the Arduino core (or something like it) so there is no need to manually define them
+
+//Code to verify this finding
+// Serial.begin(115200);
+//   Serial.println(F("\nI2C PINS"));
+//   Serial.print(F("\tSDA = ")); Serial.println(SDA);
+//   Serial.print(F("\tSCL = ")); Serial.println(SCL);
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
