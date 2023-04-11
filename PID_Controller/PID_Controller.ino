66b97aaa05b2ed4f842ee028fbf4e6be9305be49
Moo228 <92613774+Moo228@users.noreply.github.com>
2023-04-11 08:30:29 -0600

    progress on refactoring

--- PID_Controller/PID_Controller.ino
+++ PID_Controller/PID_Controller.ino
@@ -1,10 +1,10 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-//  Title: Control System 2D Test Board
+//  Title: Herculift Device Control System
 //  Capstone Team: 37 
 //  Authors: Tyler Dickson, Joseph Edmund, Isaac Sorensen
 //  
-//  Description: This is a body of code to test the controls system of the wearable lift 
+//  Description: This is a body of code implementing the controls system of the wearable lift 
 //    assist device (Herculift Device) by using a 2D mockup of the system.
 //  
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -17,68 +17,46 @@
 
 //Per the Arduino Nano documentation, pins 3, 5, 6, 9, 10, and 11 support pulse width modulation (PWM).
 //The PWM pins 5 and 6 are connected to a 16-bit timer and run at a default frequency of 976.56Hz.
-//The PWM pins 3, and 11 are connected to Timer 0? in the ATmega168p microcontroller chip.
-//The PWM pins 9, and 10 are connected to Timer 2? in the ATmega168p microcontroller chip.
-//This will be important for modifying the default frequencies supported by the PWM pins (controlled by the timers).
 
-//Pins connected to L298N motor controller.
+//Pins connected to the motor controller.
 #define MOTOR_DIRECTION_PIN 2
 #define ENABLE_PIN 3
 #define READY_PIN 4
 #define MOTOR_PWM_INPUT_PIN 5
 
-
-//Pin for strain gauge amplifier for the load. Pin 16 corresponds to pin A3.
-#define INPUT_PIN_LOAD 17
-
-//Pin for strain gauge amplifier for the cable. Pin 17 corresponds to pin A2.
+//Pin for strain gauge amplifier for the cable. Pin 16 corresponds to pin A2.
 #define INPUT_PIN_CABLE 16
 
-//Define values in which errorToPWM() will change functionality.
-#define ERROR_ZONE_VAL 0.05
-#define MOVE_ZONE_VAL 0.25
+//Pin for strain gauge amplifier for the load. Pin 17 corresponds to pin A3.
+#define INPUT_PIN_LOAD 17
 
-#define MAX_MOTOR_PWM 150
-#define MIN_MOTOR_PWM 20
-
+// #define MAX_MOTOR_PWM 150
+// #define MIN_MOTOR_PWM 20
 
 //Define a number to use as the scaled value of the load tension.
 #define WEIGHT_ASSIST_FACTOR 0.75
 
 /****************************************Variables****************************************/
 
-//Define variables for load cell readings & calibration factors.
+//Define variables for load cell readings.
 double load_scale_reading; // load cell value
 double cable_scale_reading; // load cell value
-double tension_error;       // our defined error value
-double timeVar; // variable to track time
-int currentDirection;
+double tension_error; // our defined error value
 
-//Create a variable to hold the value of the weight of the load. It will be scaled by WEIGHT_ASSIST_FACTOR.
-double scaledTensionHandle;
+int currentDirection;
 
 //Enum to simplify motor movement.
 enum MotorMotion{UP, DOWN, NONE};
 
-//Enum to categorize the motor movement regime.
-enum MovementRegime{MAX_UP_REGIME, UP_REGIME, NONE_REGIME, DOWN_REGIME, MAX_DOWN_REGIME};
+//Define the variables we'll be connecting to
+double setpoint, input, output_UP, output_DOWN;
 
+//Specify the links and initial tuning parameters
+double Kp_Up=.5, Kd_Up=0.0, Ki_Up=0.0;
+double Kp_Down=.5, Kd_Down=0.0, Ki_Down=0.0;
+PID myPID_UP(&Input, &Output_UP, &Setpoint, Kp_Up, Ki_Up, Kd_Up, DIRECT);
+PID myPID_DOWN(&Input, &Output_DOWN, &Setpoint, Kp_Down, Ki_Down, Kd_Down, REVERSE);
 /****************************************Function Declarations****************************************/
-
-// This is the built-in Arduno map function but with the double data type instead of the long data type.
-//Parameters:
-//  x- the number to map.
-//  in_min- the lower bound of the value's current range.
-//  in_max- the upper bound of the value's current range.
-//  out_in- the lower bound of the value's target range.
-//  out_max- the upper bound of the value's target range.
-double mapDouble(double x, double in_min, double in_max, double out_min, double out_max);
-
-//This function wraps the logic of motor control into a simple command.
-//Parameters:
-//  direction- uses the MotorMotion enum to specify UP, DOWN, or NONE.
-//  dutyCycle- specify the duty cycle to run the motors at.
-void moveMotor(MotorMotion direction, int dutyCycle);
 
 //This function simply calculates the difference between the tension measured by the load and the tension measured by the cable. 
 //In other words, the error is the difference between the desired tension (cable value) and the actual tension (scaled load value).
@@ -94,22 +72,11 @@ double calculateError(double scaledTensionHandle, double tensionCable);
 //  errorVal- the value returned from calculateError().
 enum MovementRegime errorToPWM(double errorVal);
 
-/**************************************PID********************************************/
-
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output_UP, Output_DOWN;
-
-//Specify the links and initial tuning parameters
-double Kp_Up=.5, Kd_Up=0.0, Ki_Up=0.0;
-double Kp_Down=.5, Kd_Down=0.0, Ki_Down=0.0;
-PID myPID_UP(&Input, &Output_UP, &Setpoint, Kp_Up, Ki_Up, Kd_Up, DIRECT);
-PID myPID_DOWN(&Input, &Output_DOWN, &Setpoint, Kp_Down, Ki_Down, Kd_Down, REVERSE);
-
 /****************************************Main****************************************/
 
 void setup() {
   //Set the pins as output.
-  //These pins will control the motors by interfacing with the L298N Motor controller.
+  //These pins will control the motors by interfacing with the motor controller.
   pinMode(MOTOR_PWM_INPUT_PIN, OUTPUT);
   pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
@@ -127,49 +94,42 @@ void setup() {
   }
   analogReference(INTERNAL); // Set the internal reference voltage to 1.1V to increase resolution on force sensors
 
-  //Read and normalize the values from the strain gauges. A decimal percentage of 
+  //Read and normalize the values from the strain gauges.
   load_scale_reading = analogRead(INPUT_PIN_LOAD);
   cable_scale_reading = analogRead(INPUT_PIN_CABLE);
   tension_error = calculateError(load_scale_reading, cable_scale_reading);
   
-   //initialize the variables we're linked to
-  Input = cable_scale_reading;
-  Setpoint = load_scale_reading * WEIGHT_ASSIST_FACTOR;
+  //Initialize the variables we're linked to
+  input = tension_error;
+  setpoint = 0;
 
-  //turn the PID on
+  //Turn the PID on
   myPID_UP.SetMode(AUTOMATIC);
-
-  //turn the PID on
   myPID_DOWN.SetMode(AUTOMATIC);
 }
 
 void loop() {
-  // unsigned long startTime = millis(); // Record the start time
-  
   //Read and normalize the values from the strain gauges. A decimal percentage of 
   load_scale_reading = analogRead(INPUT_PIN_LOAD);
   cable_scale_reading = analogRead(INPUT_PIN_CABLE);
   tension_error = calculateError(load_scale_reading, cable_scale_reading);
 
-  //Update the variables we're linked to
-  Input = cable_scale_reading;
-  Setpoint = load_scale_reading * WEIGHT_ASSIST_FACTOR;
-
-  if (tension_error > 0){
+  input = tension_error;
+  if (tension_error > 0) {
     myPID_UP.Compute();
-    moveMotor(UP,Output_UP);
+    moveMotor(UP, output_UP);
     currentDirection = 1;
     
   }
   else {
     myPID_DOWN.Compute();
-    moveMotor(DOWN,Output_DOWN);
+    moveMotor(DOWN, output_DOWN);
     currentDirection = -1;
   }
   
-  
-//  analogWrite(PIN_OUTPUT, Output);
+  // myPID.Compute();
 
+  //Serial Output
   Serial.print("Load:");
   Serial.print(load_scale_reading, 3);
   Serial.print(", ");
@@ -178,109 +138,15 @@ void loop() {
   Serial.print(", ");
   Serial.print("Error:");
   Serial.print(tension_error);
-////  Serial.print(", ");
-////  Serial.print("Output:");
-//  if (currentDirection == 1){
-////    Serial.print(Output_UP);
-//  }
-//  else{
-////    Serial.print(Output_DOWN);
-//  }
-////  Serial.print(", ");
-////  Serial.print("Dir:");
-  Serial.println(", ");
- 
-  
-  //Adjust the motor based on the read handle sensor data.
-//  enum MovementRegime movementRegime = errorToPWM(tension_error);
-//  Serial.print(", ");
-//  Serial.print("Regime:");
-//  Serial.println((double) movementRegime/10.0);
+  // Serial.print(", ");
+  // Serial.print("Output:");
+  // Serial.println(output);
 
   delayMicroseconds(100); // Wait and increase the sampling rate
 }
 
 /****************************************Function Definitions****************************************/
 
-double mapDouble(double x, double in_min, double in_max, double out_min, double out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void moveMotor(MotorMotion direction, int dutyCycle) {
-    switch (direction) {
-      case UP:
-        //When going UP put the direction pin of the motor control driver LOW.
-        analogWrite(MOTOR_PWM_INPUT_PIN, dutyCycle);
-        digitalWrite(MOTOR_DIRECTION_PIN, LOW);
-        break;
-      case DOWN:
-        //When going UP put the direction pin of the motor control driver HIGH.
-        analogWrite(MOTOR_PWM_INPUT_PIN, dutyCycle);
-        digitalWrite(MOTOR_DIRECTION_PIN, HIGH);
-        break;
-      case NONE:
-        //The driver naturally pulls the pin high so put the direction pin HIGH.
-        analogWrite(MOTOR_PWM_INPUT_PIN, 0);
-        digitalWrite(MOTOR_DIRECTION_PIN, HIGH);
-        break;
-    }
-}
-
 double calculateError(double scaledTensionHandle, double tensionCable) {
   return WEIGHT_ASSIST_FACTOR * scaledTensionHandle - tensionCable;
-}
-//                              PWM
-//                              ||
-// ________________             ||             ________________<- MAX DUTY CYCLE
-//                |\            ||            /|               
-//                | \           ||           / |               
-//                |  \          ||          /  |               
-//                |   \         ||         /   |               
-//                |    \        ||        /    |               
-//                |     \       ||       /     |               
-//                |      \______||______/<-----|----------------- MIN DUTY CYCLE (0)         
-// ===============|======|======||======|======|=============== Error
-//                |      |      ||      |      |
-//             -MOVE  -ERROR          ERROR   MOVE
-//              ZONE   ZONE           ZONE    ZONE 
-//              VAL    VAL            VAL     VAL  
-
- enum MovementRegime errorToPWM(double errorVal) {
-  if(errorVal < -MOVE_ZONE_VAL) {
-    //Move the motor down at the max value when the error value is too negetive (Safety feature to prevent excessive motor speed).
-    moveMotor(DOWN, MAX_MOTOR_PWM);
-    // Serial.println("MAX DOWN regime");
-    return MAX_DOWN_REGIME;
-  } else if(errorVal > -MOVE_ZONE_VAL && errorVal < -ERROR_ZONE_VAL) {
-    //Move the motor down at a linear rate between the (x, y) points (-MOVE_ZONE_VAL, MAX_MOTOR_PWM) and (-ERROR_ZONE_VAL, 0).
-    double mappedDouble = mapDouble(errorVal, -MOVE_ZONE_VAL, -ERROR_ZONE_VAL, MAX_MOTOR_PWM, MIN_MOTOR_PWM);
-                // double mapDouble(double x, double in_min, double in_max, double out_min, double out_max)
-                // return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    
-    // Serial.
-    // Serial.print(mappedDouble);
-    moveMotor(DOWN,mappedDouble);
-    // moveMotor(DOWN, 50);
-    // Serial.println("DOWN regime");
-    return DOWN_REGIME;
-  } else if(errorVal > -ERROR_ZONE_VAL && errorVal < ERROR_ZONE_VAL) {
-    //Stop the motor when errorVal is within a certain margin of error defined by 2 * ERROR_ZONE_VAL.
-    moveMotor(NONE, 0);
-    // Serial.println("NONE regime");
-    return NONE_REGIME;
-  } else if(errorVal > ERROR_ZONE_VAL && errorVal < MOVE_ZONE_VAL) {
-    //Move the motor down at a linear rate between the (x, y) points (ERROR_ZONE_VAL, 0) and (MOVE_ZONE_VAL, MAX_MOTOR_PWM).
-    double mappedDouble = mapDouble(errorVal, ERROR_ZONE_VAL, MOVE_ZONE_VAL, MIN_MOTOR_PWM, MAX_MOTOR_PWM);
-    // Serial.print(mappedDouble);
-    moveMotor(UP, mappedDouble);
-    // moveMotor(UP, 50);
-    // Serial.println("UP regime");
-    return UP_REGIME;
-  } else {
-    //Move the motor down at the max value when the error value is too positive (Safety feature to prevent excessive motor speed).
-    moveMotor(UP, MAX_MOTOR_PWM);
-    // Serial.println("MAX UP regime");
-    return MAX_UP_REGIME;
-  }
 }
